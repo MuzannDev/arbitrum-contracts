1b874c6aa29e16d0afd5034cccf3eb11c611872f
Muz <muz@Muzs-MacBook-Pro.local>
2023-03-21 18:59:00 +0100

    fix

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,7 @@ module.exports = {
   solidity: "0.8.18",
   networks: {
     arbitrum:{
-      url: "https://arb-mainnet.g.alchemy.com/v2/PrToIvsX83wdgzTw6V0-nipEzTurWRK7",
+      url: "",
       accounts: [process.env.OPEN_SOURCE_PKEY]
     }
   }
